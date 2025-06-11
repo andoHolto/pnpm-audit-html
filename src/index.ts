500c7fd3cd837149ea6e71a2fb7c149c0c4f52bd
Andy Holton <102529538+andoHolto@users.noreply.github.com>
2025-06-11 04:32:51 +0000

    modify to accept sev level and prod switches

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,16 @@ import { execSync, ExecSyncOptionsWithStringEncoding } from 'child_process';
 import { generateHtml } from './htmlGenerator';
 import { writeFileSync } from 'fs';
 
-export const runPnpmAudit = (): string => {
+const SEVERITY_LEVELS: Array<string> = ['info', 'low', 'moderate', 'high', 'critical'] as const 
+
+export const runPnpmAudit = (auditLevel: string, prod: boolean): string => {
+  const levelIncludes = SEVERITY_LEVELS.slice(SEVERITY_LEVELS.indexOf(auditLevel)).join("\" or .value.severity == \"")
+  const auditLevelPruneJQ = `| jq '.advisories = (.advisories | to_entries | 
+    map(select(.value.severity == \"${levelIncludes}\")) | 
+    from_entries)'`
+
   try {
-    return execSync('pnpm audit --json', {
+    return execSync(`pnpm audit --audit-level ${auditLevel} ${prod===true ? '--prod' : ''} --json ${auditLevelPruneJQ}`, {
       encoding: 'utf-8',
       stdio: 'pipe',
       maxBuffer: 1024 * 1024 * 20, // 20 MB buffer
@@ -46,11 +53,13 @@ export const main = (cliArgs: string[] = process.argv): void => {
     .version('1.0.0')
     .description('Generate HTML report from pnpm audit')
     .option('-o, --output <file>', 'Output HTML file', 'pnpm-audit-report.html')
+    .option('--audit-level <severity>', 'Minimum severity', 'high')
+    .option('--prod', 'Only audit prod', false)
     .action((options) => {
       try {
-        console.log('Running pnpm audit...');
+        console.log(`Running pnpm audit with options [--audit-level=${options.auditLevel}, --prod=${options.prod}] ...`);
         console.time('Audit report generation time');
-        const auditOutput = runPnpmAudit();
+        const auditOutput = runPnpmAudit(options.auditLevel, options.prod);
         console.log('Audit command completed. Parsing output...');
         generateAuditReport(auditOutput, options.output);
 
